bb003e97f59052f71dc99b2191169121e15e0315
Ezgi <ezgidemir824@gmail.com>
2025-07-18 06:24:05 +0300

    Merged local project with remote

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ],
+  css: ['../assets/css/main.css'],
+
+  plugins: [
+    '../plugins/socket.client.ts',
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
